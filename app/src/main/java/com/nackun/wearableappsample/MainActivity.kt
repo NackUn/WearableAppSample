a88653c6f8f5d382a5477b2ff6f266c881f1199b
NackUn <king4521@naver.com>
2021-12-27 23:54:02 +0900

    compose 셋팅

--- app/src/main/java/com/nackun/wearableappsample/MainActivity.kt
+++ app/src/main/java/com/nackun/wearableappsample/MainActivity.kt
@@ -1,18 +1,61 @@
 package com.nackun.wearableappsample
 
-import android.app.Activity
 import android.os.Bundle
-import com.nackun.wearableappsample.databinding.ActivityMainBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.Text
+import com.nackun.wearableappsample.theme.WearableAppSampleTheme
 
-class MainActivity : Activity() {
-
-    private lateinit var binding: ActivityMainBinding
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContent {
+            WearApp("Android")
+        }
+    }
 
+
+    @Composable
+    fun WearApp(greetingName: String) {
+
+        WearableAppSampleTheme {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colors.background),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Greeting(greetingName = greetingName)
+            }
+        }
+    }
+
+    @Composable
+    fun Greeting(greetingName: String) {
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            color = MaterialTheme.colors.primary,
+            text = stringResource(R.string.hello_world, greetingName)
+        )
+    }
+
+    @Preview
+    @Composable
+    fun DefaultPreview() {
+        WearApp("Preview Android")
     }
 }
