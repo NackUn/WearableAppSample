6fbae4179248467e42d6fee3afb5c68aba31c36c
NackUn <king4521@naver.com>
2022-01-01 22:54:14 +0900

    [phone] Timber 추가

---
+++ phone/src/main/java/nackun/sample/phone/MainApplication.kt
@@ -0,0 +1,14 @@
+package nackun.sample.phone
+
+import android.app.Application
+import timber.log.Timber
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
