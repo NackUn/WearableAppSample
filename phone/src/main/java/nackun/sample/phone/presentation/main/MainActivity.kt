f097852e03de2b258f586f74bc82ac51bbdc934c
NackUn <king4521@naver.com>
2022-01-01 23:21:31 +0900

    [phone] message 전송 추가

--- phone/src/main/java/nackun/sample/phone/presentation/main/MainActivity.kt
+++ phone/src/main/java/nackun/sample/phone/presentation/main/MainActivity.kt
@@ -6,18 +6,66 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
+import com.google.android.gms.wearable.Wearable
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 import nackun.sample.phone.ui.theme.WearableAppSampleTheme
+import timber.log.Timber
 
 class MainActivity : ComponentActivity() {
     private val mainViewModel: MainViewModel by viewModels()
 
+    private val nodeClient by lazy { Wearable.getNodeClient(this) }
+    private val messageClient by lazy { Wearable.getMessageClient(this) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        initObserve()
         setContent {
             WearableAppSampleTheme {
                 MainApp(mainViewModel)
             }
         }
+    }
+
+    private fun initObserve() {
+        lifecycleScope.launch {
+            mainViewModel.buttonClickEvent.collect {
+                Timber.d("mainViewModel.buttonClickEvent.collect")
+                startWearableActivity()
+            }
+        }
+    }
+
+    private fun startWearableActivity() {
+        lifecycleScope.launch {
+            try {
+                val nodes = nodeClient.connectedNodes.await()
+
+                // Send a message to all nodes in parallel
+                nodes.map { node ->
+                    async {
+                        messageClient.sendMessage(node.id, START_ACTIVITY_PATH, byteArrayOf())
+                            .await()
+                    }
+                }.awaitAll()
+
+                Timber.d("Starting activity requests sent successfully")
+            } catch (cancellationException: CancellationException) {
+                throw cancellationException
+            } catch (exception: Exception) {
+                Timber.d("Starting activity failed: $exception")
+            }
+        }
+    }
+
+    companion object {
+        private const val START_ACTIVITY_PATH = "/start-activity"
     }
 }
 
--- phone/src/main/java/nackun/sample/phone/presentation/main/MainViewModel.kt
+++ phone/src/main/java/nackun/sample/phone/presentation/main/MainViewModel.kt
@@ -1,10 +1,26 @@
 package nackun.sample.phone.presentation.main
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 import timber.log.Timber
 
 open class MainViewModel : ViewModel() {
+    private val _buttonClickEvent: MutableSharedFlow<Unit> =
+        MutableSharedFlow(
+            replay = 0,
+            extraBufferCapacity = 1,
+            onBufferOverflow = BufferOverflow.DROP_OLDEST
+        )
+    val buttonClickEvent = _buttonClickEvent.asSharedFlow()
+
     fun onButtonClick() {
         Timber.d("onClick")
+        viewModelScope.launch {
+            _buttonClickEvent.emit(Unit)
+        }
     }
 }
